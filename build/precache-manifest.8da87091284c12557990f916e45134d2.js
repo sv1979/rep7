028fd79d4e067e2ae10ec18fd7b0881d45f013e3
sv1979 <sergeynew37@gmail.com>
2019-01-23 11:56:03 +1300

    Add build dir

---
+++ build/precache-manifest.8da87091284c12557990f916e45134d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce27e02a89b430486120",
+    "url": "/static/js/main.ce27e02a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ce27e02a89b430486120",
+    "url": "/static/css/main.e25da230.chunk.css"
+  },
+  {
+    "revision": "bcc0b556f6be17ad904eea1bf712f2d5",
+    "url": "/index.html"
+  }
+];
